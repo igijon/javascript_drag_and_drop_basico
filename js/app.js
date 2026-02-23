4424649c7c1a8e0df58505e9043c75ff1dc9edbf
Inma Gijón <igijoninf@gmail.com>
2026-02-23 16:33:10 +0100

    Modificaciones añadiendo dragend y dragleave

--- js/app.js
+++ js/app.js
@@ -3,7 +3,12 @@ const container2 = document.getElementById("container2");
 
 // Cuando empieza el drag
 item.addEventListener("dragstart", (e) => {
+  item.classList.add("dragging");
   e.dataTransfer.setData("idArrastrable", item.id);
+});
+
+item.addEventListener("dragend", (e) => {
+  item.classList.remove("dragging");
 });
 
 // Permitir drop en el destino
@@ -12,6 +17,9 @@ container2.addEventListener("dragover", (e) => {
   container2.classList.add("over");
 });
 
+container2.addEventListener("dragleave", (e) => {
+  container2.classList.remove("over");
+});
 
 // Cuando se suelta el elemento
 container2.addEventListener("drop", (e) => {
